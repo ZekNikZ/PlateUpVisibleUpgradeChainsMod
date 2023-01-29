99c5d2124e5ad0523ea140550a1093c159919277
Matthew McCaskill <mattrmccaskill@gmail.com>
2023-01-28 19:25:47 -0600

    Initial commit

---
+++ Mod.cs
@@ -0,0 +1,57 @@
+﻿using Kitchen;
+using KitchenMods;
+using System.Reflection;
+using UnityEngine;
+using HarmonyLib;
+
+namespace KitchenVisibleUpgradeChainsMod
+{
+    public class Mod : GenericSystemBase, IModInitializer, IModSystem
+    {
+        public const string MOD_GUID = "io.zkz.plateup.visibleupgradechains";
+        public const string MOD_NAME = "Visible Upgrade Chains";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "ZekNikZ";
+        public const string MOD_GAMEVERSION = ">=1.1.3";
+
+        private static Harmony HarmonyInstance;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+        }
+
+        protected override void OnUpdate()
+        {
+           
+        }
+
+        public void PostActivate(KitchenMods.Mod mod)
+        {
+            HarmonyInstance = new Harmony(MOD_GUID);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void PreInject()
+        {
+
+        }
+
+        public void PostInject()
+        {
+            
+        }
+
+        #region Logging
+        public static void LogInfo(string _log) { Debug.Log($"[{MOD_NAME}] " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"[{MOD_NAME}] " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"[{MOD_NAME}] " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+
+        #endregion
+    }
+}
---
+++ Patches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenData;
+using System.Linq;
+
+namespace KitchenVisibleUpgradeChainsMod
+{
+    [HarmonyPatch(typeof(ApplianceInfoView), "UpdateData")]
+    class UpdateDataPatch {
+        internal static int ID;
+
+        static void Prefix(ApplianceInfoView.ViewData data)
+        {
+            ID = data.ID;
+        }
+    }
+
+    [HarmonyPatch(typeof(ApplianceInfoView), "AddTag")]
+    class AddTagPatch
+    {
+        static void Postfix(float offset, string tag, ref ApplianceInfoView __instance, ref float __result)
+        {
+            if (tag != GameData.Main.GlobalLocalisation["Upgradable"] || !GameData.Main.TryGet<Appliance>(UpdateDataPatch.ID, out var appliance) || !appliance.HasUpgrades)
+            {
+                return;
+            }
+
+            // private float AddSection(float offset, Appliance.Section details, bool centre = false)
+            var mAddTag = typeof(ApplianceInfoView).GetMethod("AddSection", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+            var details = new Appliance.Section
+            {
+                Title = "Possible Upgrades",
+                Description = string.Join(", ", appliance.Upgrades.Select(appliance => appliance.Name))
+            };
+
+            var res = (float) mAddTag.Invoke(__instance, new object[] { offset + __result, details, true });
+
+            __result += res;
+        }
+    }
+}
